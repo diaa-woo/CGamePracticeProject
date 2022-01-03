10de79488f9a71eb7bc2dc741543c575f4019a30
diadntjr <dntjr0501@gmail.com>
2022-01-03 17:48:53 +0900

    Update Code
    Shoot bullet
    Move charater use to direction key
    Modified Header file

--- CGameProject1/main.c
+++ CGameProject1/main.c
@@ -1,11 +1,5 @@
 #include "main.h"
 
-typedef enum { NOCURSOR, SOLIDCURSOR, NORMALCURSOR } CURSOR_TYPE;
-const int refreshRate = 50;  
-/*
-	1초에 몇번 콘솔입력을 받을 것인지 루틴을 정하는 변수
-*/
-
 void setcursortype(CURSOR_TYPE c) {
 	CONSOLE_CURSOR_INFO CurInfo;
 	/*
@@ -49,18 +43,8 @@ void setcursortype(CURSOR_TYPE c) {
 	*/
 }
 
-void End() {
-	system("cls");
-	gotoxy(30, 10);
-	printf("===============================GAME OVER===============================\n\n");
-	gotoxy(58, 15);
-	printf("점수=%d\n\n\n\n\n\n\n\n\n\n\n", Score);
-	setcursortype(NORMALCURSOR);
-	getchar();
-}
-
 void gotoxy(int x, int y) {
-	COORD CursorPosition = { x,y };  
+	COORD CursorPosition = { x,y };
 	/*
 		COORD는 윈도우 환경, 콘솔 화면 버퍼에서 문자 셀의 좌표 지정해주는 구조체
 		구조체로 지정할 필요 없이 CursorPosition으로 한번에 x,y 값을 지정해줄 수 있음.
@@ -68,25 +52,37 @@ void gotoxy(int x, int y) {
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), CursorPosition);
 	/*
 		응용프로그램이 직접 커널 부분의 메모리를 참조할 시 리스크 발생 가능, 이에 주소를 사용하는 포인터 대신 핸들을 사용
-		
+
 		핸들(Handle): 운영체제 내부에 있는 어떤 리소스의 주소를 정수로 치환한 값
-		
+
 		운영체제는 리소스의 주소와 핸들 값을 한 쌍으로 묶어 관리하는 '핸들 테이블' 보유
 		응용프로그램에서 이 테이블에 핸들 값을 전송해 주고, 이 테이블에서 해시한 다음, 해당 테이블 주소에 들어있는 리소스의 주소로 이동해 줌.
-		
+
 		Win32 프로그램에서는 핸들 값을 저장할 경우 HANDLE라는 자료형을 기본적으로 사용, 이 자료형의 크기는 32비트 운영체제일 경우 'unsigned int' 자료형과 동일
 
 		'GetStdHandle'는 핸들 값을 반환해주는 함수이며, 'STD_OUTPUT_HANDLE'는 표준콘솔출력의 핸들을 의미
-		
+
 		결론으로, 저 코드는 위치값과 콘솔 핸들을 해당 함수에 전달해주어 콘솔의 위치를 이동시켜줌.
 	*/
 }
 
-
+void End() {
+	system("cls");
+	gotoxy(30, 10);
+	printf("==========================GAME OVER============================\n\n");
+	gotoxy(58, 15);
+	printf("점수=%d\n\n\n\n\n\n\n\n\n\n\n", score);
+	setcursortype(NORMALCURSOR);
+	getchar();
+	return;
+}
 
 int main() {
 	setcursortype(NOCURSOR);
 	int x = 0, y = 0;
+	int fx = 0, fy = 0;
+	int bx = 0, by = 0;
+	boolean checkShoot = 0;
 	int cnt = 1;
 	/*
 		위 변수는 카운터를 의미함
@@ -120,23 +116,59 @@ int main() {
 			값은 받지만 그 입력값을 콘솔에 출력해주지는 않음
 			즉각적인 반응을 해야할 때 필요함
 		*/
-		switch (ch)
+		switch (ch)  //키 입력 처리
 		{
-		case 'w':
-			if (myPlayer.y > 0) myPlayer.y--;
+		case 72:
+			if (myPlayer.y > 0) {
+				myPlayer.y--;
+				fy--;
+			}
 			break;
-		case 's':
-			if (myPlayer.y < 24) myPlayer.y++;
+		case 80:
+			if (myPlayer.y < 24) {
+				myPlayer.y++;
+				fy++;
+			}
 			break;
-		case 'a':
-			if (myPlayer.x > 0) myPlayer.x--;
+		case 75: 
+			if (myPlayer.x > 0) {
+				myPlayer.x--;
+				fx--;
+			}
 			break;
-		case 'd':
-			if (myPlayer.x < 79) myPlayer.x++;
+		case 77:
+			if (myPlayer.x < 79) {
+				myPlayer.x++;
+				fx++;
+			}
+			break;
+		case ' ':
+			if (checkShoot == 0) {
+				checkShoot = 1;
+				bx = fx;
+				by = fy;
+			}
 			break;
 		case ESC:
 			End();
 			return 0;
+		}
+
+		if (checkShoot == 1) {
+			gotoxy(bx, by);
+			putch(' ');
+			/*
+				한 글자만 출력하여 준다. 
+				얘를 뭣하러 쓰나 싶겠지만 우선 버퍼를 거치지 않고 바로 출력하기에 출력 속도가 상당히 빠르고, 많은 기능을 포함하고 있는 printf에 비해 더 빠른 속도를 나타내어 준다
+			*/
+			if (by == 0) {
+				checkShoot = 0;
+			}
+			else {
+				by--;
+				gotoxy(bx, by);
+				putch('i');
+			}
 		}
 		gotoxy(myPlayer.x, myPlayer.y);
 		printf("@");
--- CGameProject1/main.h
+++ CGameProject1/main.h
@@ -5,6 +5,13 @@
 #include <conio.h>  
 /*
 	conio.h는 콘솔 입력 함수를 호출시켜 준다.
+	옛날 MS-DOS시절부터 사용된 헤더 파일이며, 현재는 C언어 표준도 아니고 POSIX 함수도 아니기에, Visual Studio에서만 사용 가능(리눅스, OS X 사용 불가)
+	
+	POSIX(Portable Operating System Interface + X(UNIX의 X): 유닉스의 애플리케이션 프로그래밍 인터페이스(Application Programing Interface, API) 규격, 위 규격에 따르면 유닉스와 직접적인 연관이 없어도 유닉스 호환(Unix-like) 운영체제라고 부름
+	예시로, 리눅스는 유닉스와는 별도로 개발되었지만 POSIX 표준을 거의 다 준수하기 때문에 리눅스는 유닉스 호환 운영체제로 봄(공식인증을 잘 안해줘서, 그냥 넓게 보면 호환 운영 체제라 침)
+	macOS도 POSIX를 따름. macOS는 10.5 버젼부터 정식 인증을 받음
+
+	C언어에는 POSIX를 따르는 헤더 파일과 함수들이 일부 존재하는데, 이 개수가 상당하다. 다만 이 POSIX를 따르지 않다고 해서 다른 헤더파일들은 POSIX 규격을 따르는 운영체제에서 따르지 못한다는 소리는 아니다.
 */
 
 #define MAXBALL 20
@@ -22,7 +29,11 @@ typedef struct {
 
 const char* arEnemy[] = { ";:^:; ", " zZWZz ", " o0@0o ", " <-=-> " };
 
+typedef enum { NOCURSOR, SOLIDCURSOR, NORMALCURSOR } CURSOR_TYPE; 
+
 int score;
+
+const int refreshRate = 25;
 
 struct tag_Ball {
 	boolean ifExist;
@@ -31,3 +42,8 @@ struct tag_Ball {
 	int nStay;
 }Ball[MAXBALL];
 
+void gotoxy(int x, int y);  //콘솔 이동 함수
+
+void End();  //게임 엔딩 함수
+
+void setcursortype(CURSOR_TYPE c);  //커서 타입 변경 함수
